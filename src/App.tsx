b1c262b92361261588afe4d492d0f2887c7894f6
Michael Montaque <montaque.developer@gmail.com>
2023-05-17 00:22:29 -0700

    Mobile-Friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { Home } from "./pages/Home/home";
 import { STier } from "./pages/S-Tier/stier";
+import classnames from "classnames";
 import logo from "./images/Chaperone-splash.png";
 import "./App.scss";
 import { AIMasterClass } from "./pages/AI-MasterClass/AIMasterClass";
@@ -12,22 +13,48 @@ enum URLRoutes {
   S_Tier = "stier",
 }
 
-function App() {
+// Create a condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+// // Initial check
+// handleTabletChange(mediaQuery);
+
+const App = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+  const navClass = classnames({ "app__left-nav--open": isMenuVisible });
+
+  useEffect(() => {
+    mediaQuery.addEventListener("change", handleTabletChange);
+  }, []);
+
+  const handleTabletChange = (e: any) => {
+    // Check if the media query is true
+    if (!e.matches) {
+      setIsMenuVisible(false);
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <div className="app text-slate-100">
-      <header className="app__header bg-slate-700">
-        <a href="/" className="uppercase text-l font-sans font-extrabold">
+      <header className="app__header bg-slate-700 justify-between flex">
+        <a href="/" className="uppercase text-l font-sans font-extrabold ">
           <img
-            className="max-w-xs"
+            className="app__logo"
             src={logo}
             alt="Technithusiast Chaperone."
           />
         </a>
+        <button className="app__menu-btn" onClick={toggleMenu}>
+          Menu
+        </button>
       </header>
 
       <div className="app__container">
-        <div className="app__left-nav">
-          <h3 className="text-lg mb-2">Navigation</h3>
+        <div className={`app__left-nav ${navClass}`}>
           <ul className="rounded border border-slate-600 bg-slate-700">
             <li className="w-full bg-slate-600 hover:bg-slate-500 active:bg-slate-800 border-b-s;-500">
               <Link
@@ -63,6 +90,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
